1deb0a8e62f09e3cc10561f4abcea49d49ba54d0
Samuel Rowan <samuel@samuelrowan.com>
2020-02-01 19:13:57 -0600

    changes

--- bvpb/gatsby-config.js
+++ bvpb/gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    title: `Berryville Portable Buildings`,
+    description: `Cool buildings that happen to be portable.`,
     author: `@gatsbyjs`,
   },
   plugins: [
--- bvpb/src/components/header.js
+++ bvpb/src/components/header.js
@@ -1,19 +1,20 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
+import { jssPreset } from "@material-ui/styles";
 
 const Header = ({ siteTitle }) => (
-  <header
+  <AppBar
     style={{
       background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
+      marginBottom: `1rem`,
     }}
   >
     <div
       style={{
         margin: `0 auto`,
         maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
+        padding: `0.45rem 0.0875rem`,
       }}
     >
       <h1 style={{ margin: 0 }}>
@@ -28,7 +29,7 @@ const Header = ({ siteTitle }) => (
         </Link>
       </h1>
     </div>
-  </header>
+  </AppBar>
 )
 
 Header.propTypes = {
